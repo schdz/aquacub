3aca21624f59132063657af5da427defeac961c0
s.chesnokov <s.chesnokov@qiwi.com>
2021-06-24 19:11:37 +0300

    1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+let text = process.argv[2];
+if(!text){
+    text = "!!!";
+}
+ 
+app.get("/", function(request, response){
+     response.end(`aqua cube ${text}!`);
+});
+
+app.listen(80);
